df16c8b5e29954b90da296c47f237eee740d1575
kaveer rajcoomar <kaveer.rajcoomar@hotmail.com>
2020-01-10 17:25:01 +0400

    remove using

--- OccupetionalTherapy/RangeOfMotion.cs
+++ OccupetionalTherapy/RangeOfMotion.cs
@@ -2,13 +2,6 @@
 using Model;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace OccupetionalTherapy
